57b7b9b061b3cdec131d16c8cc3dc661ac204bee
demojuly24gcp <edurekajuly24gcp@gmail.com>
2024-07-20 18:02:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "tt-githubdemo-bucket-001"
-  project                  = "tt-dev-001"
+  project                  = "jenkinsdemo-430011"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "my-bucket" {
 #
 resource "google_storage_bucket" "my-bucket2" {
   name                     = "tt-githubdemo-bucket-002"
-  project                  = "tt-dev-001"
+  project                  = "jenkinsdemo-430011"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
