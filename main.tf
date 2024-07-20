fca096003838a9ff806977108f9ca93ef0744196
demojuly24gcp <edurekajuly24gcp@gmail.com>
2024-07-20 18:06:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "my-bucket" {
+resource "google_storage_bucket" "aj-githubdemo-bucket-002" {
   name                     = "aj-githubdemo-bucket-001"
   project                  = "jenkinsdemo-430011"
   location                 = "US"
@@ -6,7 +6,7 @@ resource "google_storage_bucket" "my-bucket" {
   public_access_prevention = "enforced"
 }
 #
-resource "google_storage_bucket" "my-bucket2" {
+resource "google_storage_bucket" "at-githubdemo-bucket-002" {
   name                     = "at-githubdemo-bucket-002"
   project                  = "jenkinsdemo-430011"
   location                 = "US"
